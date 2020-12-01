7c97a99bbcd766ad7be8312d5aeaa2624c2d03d0
HarithaML <Lakshmi.haritha@outlook.com>
2020-11-30 17:59:33 -0800

    shopping final

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,33 @@
+class SearchController < ApplicationController
+  skip_before_action :authorize
+  include CurrentCart
+  before_action :set_cart
+
+  def new
+    @keyword = 'python'
+    @products = Product.order(:name)
+  end
+
+  def create
+    @keyword = params[:keyword]
+    key = @keyword.strip.downcase
+
+    # if keywords contains nondigit char, we don't want to search price, because
+    # that's no chance to find a match there
+    if /[^\d\.]/ =~ key
+      @products = Product.where('description like ? or name like ?',
+                                %{%#{key}%},
+                                %{%#{key}%}
+      )
+    else
+      @products = Product.where('description like ? or name like ? or price = ?',
+                                %{%#{key}%},
+                                %{%#{key}%},
+                                key)
+    end
+
+    # render "search#new" directly, in this way, action "new" will not be excuted, so
+    # @keywork and @product will not be overwritten, that's what we want
+    render :new
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+
+  get 'contacts/index'
+  get 'news/index'
+  get 'question', to: "question#index"
+  get 'search', to: 'search#new'
+  post 'search', to: "search#create"
+
+  get 'admin', to: "admin#index"      # admin_path
+  get 'login', to: "access#new"       # login_path
+  post 'login', to: 'access#create'
+  delete 'logout', to: 'access#destroy' # logout
+
+  resources :users
+  resources :orders
+  resources :lineitems
+  resources :carts
+  resources :products
+
+  get 'shopper/index'                 # shopper_index_path
+  get 'shopper' , to: 'shopper#index' # shopper_path
+  root 'shopper#index', as: 'root'    # root_path
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
